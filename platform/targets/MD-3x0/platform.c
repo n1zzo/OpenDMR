257d252da2b3e0cca64322cb76136345a5a47ebb
Silvano Seva <silseva@fastwebnet.it>
2021-06-05 22:13:37 +0200

    Added initialisation of USB driver to MD-3x0 and MD-9600 platform_init()

--- platform/targets/MD-3x0/platform.c
+++ platform/targets/MD-3x0/platform.c
@@ -38,6 +38,7 @@
 #include <toneGenerator_MDx.h>
 #include <interfaces/rtc.h>
 #include <interfaces/audio.h>
+#include <interfaces/usb.h>
 
 md3x0Calib_t calibration;
 hwInfo_t hwInfo;
@@ -76,6 +77,7 @@ void platform_init()
     rtc_init();                      /* Initialise RTC                         */
     backlight_init();                /* Initialise backlight driver            */
     audio_init();                    /* Initialise audio management module     */
+    usb_init();                      /* Initialise USB driver                  */
 }
 
 void platform_terminate()
